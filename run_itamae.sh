d12cf9256c2ebbcebaac43df085cb35d5ffd3484
Eiji Hachiya <hachiyae@gmail.com>
2017-02-10 03:01:04 +0900

    ホスト名ダサいので変える

--- run_itamae.sh
+++ run_itamae.sh
@@ -2,4 +2,4 @@
 
 WORK_DIR="`pwd`/itamae"
 
-bundle exec "itamae ssh --vagrant --host itamae-jenkins --node-yaml $WORK_DIR/node.yml $WORK_DIR/roles/ci.rb"
+bundle exec "itamae ssh --vagrant --host jenkins --node-yaml $WORK_DIR/node.yml $WORK_DIR/roles/ci.rb"
